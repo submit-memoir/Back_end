c0f9629e934e90995555d7b8072d0a5271c955ea
min56 <minchaeg85@gmail.com>
2022-08-29 21:28:54 +0900

    [ ADD ] 프로젝트 생성

---
+++ submit/src/main/java/com/memoir/submit/SubmitApplication.java
@@ -0,0 +1,13 @@
+package com.memoir.submit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmitApplication.class, args);
+	}
+
+}
---
+++ submit/src/test/java/com/memoir/submit/SubmitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memoir.submit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
